a19a6a53d60a082ab3a4b376f394bf06182f6c34
Henrique Mafra <henriquemafra89@gmail.com>
2023-07-15 12:11:22 -0300

    cria a funcionalidade de adicionar tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import CardAdicionar from './components/CardAdicionar';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [tarefas, setTarefas] = useState([]);
+
+  const criarTarefa = (texto) => {
+    const novaTarefa = {
+      id: Date.now(),
+      texto: texto,
+      finalizado: false
+    };
+    setTarefas([...tarefas, novaTarefa]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Suas Tarefas</h1>
+      <CardAdicionar adicionarTarefa={criarTarefa} />
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa.id}>{tarefa.texto}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
